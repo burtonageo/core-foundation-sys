e5a6fe47ed647103927db63d4db939c778251b69
George Burton <burtonageo@gmail.com>
2016-09-05 19:17:57 +0100

    Make CFUUIDBytes fields public

--- src/uuid.rs
+++ src/uuid.rs
@@ -11,22 +11,22 @@ pub type CFUUIDRef = *const __CFUUID;
 #[repr(C)]
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 pub struct CFUUIDBytes {
-    byte0: UInt8,
-    byte1: UInt8,
-    byte2: UInt8,
-    byte3: UInt8,
-    byte4: UInt8,
-    byte5: UInt8,
-    byte6: UInt8,
-    byte7: UInt8,
-    byte8: UInt8,
-    byte9: UInt8,
-    byte10: UInt8,
-    byte11: UInt8,
-    byte12: UInt8,
-    byte13: UInt8,
-    byte14: UInt8,
-    byte15: UInt8
+    pub byte0: UInt8,
+    pub byte1: UInt8,
+    pub byte2: UInt8,
+    pub byte3: UInt8,
+    pub byte4: UInt8,
+    pub byte5: UInt8,
+    pub byte6: UInt8,
+    pub byte7: UInt8,
+    pub byte8: UInt8,
+    pub byte9: UInt8,
+    pub byte10: UInt8,
+    pub byte11: UInt8,
+    pub byte12: UInt8,
+    pub byte13: UInt8,
+    pub byte14: UInt8,
+    pub byte15: UInt8
 }
 
 extern "C" {
